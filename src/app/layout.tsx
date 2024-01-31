f150c84799521b00c09d7a6eb83536484589319f
developer-vsandhu <developervsandhu@gmail.com>
2024-01-31 14:23:14 -0500

    Responsive Design Fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
     <div
         className="hidden sm:block bg-[#fbe2e3] dark:bg-[#5b2224] z-[-1] absolute top-[-6rem] right-[11rem] h-[35.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
     <div
-        className="hidden sm:block bg-[#fbe2e3] dark:bg-[#5b2224] z-[-1] absolute top-[-6rem] left-[11rem] h-[35.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
+        className="hidden lg:block bg-[#fbe2e3] dark:bg-[#5b2224] z-[-1] absolute top-[-6rem] left-[11rem] h-[35.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
 
     <Header/>
     {children}
