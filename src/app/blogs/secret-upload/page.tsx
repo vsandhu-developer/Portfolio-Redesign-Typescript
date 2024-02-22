40ccd49db2949775ab9fdaa40da5e837542a0cf9
developer-vsandhu <developervsandhu@gmail.com>
2024-02-22 13:44:14 -0500

    Upload Blog Functionallity added

--- src/app/blogs/secret-upload/page.tsx
+++ src/app/blogs/secret-upload/page.tsx
@@ -1,3 +1,80 @@
+"use client";
+import { useRef, useState } from "react";
+import { Button } from "@/components/ui/button";
+import dynamic from "next/dynamic";
+
+const JoditEditor = dynamic(
+  () => import("jodit-react"),
+  { ssr: false } // This will prevent server-side rendering
+);
+
 export default function UploadBlog() {
-  return <h1>Uplaod Blog</h1>;
+  const initialState = {
+    title: "",
+    content: "",
+    category: "",
+  };
+  const [content, setContent] = useState("");
+  const editor = useRef(null);
+  const [title, setTitle] = useState("");
+  const [category, setCategory] = useState("");
+  const [blog, setBlog] = useState(initialState);
+
+  const categories = [
+    {
+      title: "Technology",
+    },
+    {
+      title: "Science",
+    },
+    {
+      title: "Health",
+    },
+    {
+      title: "Business",
+    },
+    {
+      title: "Entertainment",
+    },
+    {
+      title: "Sports",
+    },
+    {
+      title: "Education",
+    },
+    {
+      title: "Travel",
+    },
+    {
+      title: "Lifestyle",
+    },
+  ];
+  return (
+    <div>
+      <div className="max-w-4xl m-auto py-10">
+        <h1 className="text-4xl">
+          Upload --{" "}
+          <span className="text-sm">Share your new thoughts with other's</span>
+        </h1>
+
+        <div className="m-auto mt-10 shadow-sm px-4 bg-white py-6 rounded-md space-y-6">
+          <input className="p-3 rounded-lg w-full border" placeholder="Title" />
+          <JoditEditor
+            ref={editor}
+            value={content}
+            onChange={(newContent) => setContent(newContent)}
+          />
+          <fieldset className="space-y-4">
+            <legend>Choose Category</legend>
+            <select className="p-3 rounded-lg w-full border">
+              {categories.map((category) => (
+                <option key={category.title}>{category.title}</option>
+              ))}
+            </select>
+          </fieldset>
+          <Button className="bg-red-600">Upload Now</Button>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Jost } from "next/font/google";
+import { Jost, Poppins } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/comps/Header";
 import NextTopLoader from "nextjs-toploader";
@@ -12,8 +12,10 @@ const inter = Jost({
 
 export const metadata: Metadata = {
   title: "Vinaypartap Singh",
-  description: "- Web Developer | React.js, Node.js, MongoDB, Firebase, React Native, Shopify Liquid (Theme Development), WordPress, Elementor, Google Sites |",
-  keywords: "React Native, Laundry App, Job Finder, Image Upload, Video Upload, YouTube UI Clone, Daily1Blog, Blog Website, Full Stack Development. Full Stack Developer, Front-End Developer, Back-End Developer, Firebase, React JS, Next JS."
+  description:
+    "- Web Developer | React.js, Node.js, MongoDB, Firebase, React Native, Shopify Liquid (Theme Development), WordPress, Elementor, Google Sites |",
+  keywords:
+    "React Native, Laundry App, Job Finder, Image Upload, Video Upload, YouTube UI Clone, Daily1Blog, Blog Website, Full Stack Development. Full Stack Developer, Front-End Developer, Back-End Developer, Firebase, React JS, Next JS.",
 };
 
 export default function RootLayout({
@@ -23,29 +25,26 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-    <body className={inter.className}>
-
-    <div
-        className="hidden sm:block bg-[#fbe2e3] dark:bg-[#5b2224] z-[-1] absolute top-[-6rem] right-[11rem] h-[35.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
-    <div
-        className="hidden lg:block bg-[#fbe2e3] dark:bg-[#5b2224] z-[-1] absolute top-[-6rem] left-[11rem] h-[35.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
-    <NextTopLoader
-        color="#EF4444"
-        initialPosition={0.08}
-        crawlSpeed={500}
-        height={3}
-        crawl={true}
-        showSpinner={false}
-        easing="ease"
-        speed={200}
-        shadow="0 0 10px #EF4455,0 0 5px #EF4466"
-        zIndex={1600}
-        showAtBottom={false}
-    />
-    <Header/>
-    {children}
-    <Footer />
-    </body>
+      <body className={inter.className}>
+        <div className="hidden sm:block bg-[#fbe2e3] dark:bg-[#5b2224] z-[-1] absolute top-[-6rem] right-[11rem] h-[35.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
+        <div className="hidden lg:block bg-[#fbe2e3] dark:bg-[#5b2224] z-[-1] absolute top-[-6rem] left-[11rem] h-[35.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
+        <NextTopLoader
+          color="#EF4444"
+          initialPosition={0.08}
+          crawlSpeed={500}
+          height={3}
+          crawl={true}
+          showSpinner={false}
+          easing="ease"
+          speed={200}
+          shadow="0 0 10px #EF4455,0 0 5px #EF4466"
+          zIndex={1600}
+          showAtBottom={false}
+        />
+        <Header />
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
